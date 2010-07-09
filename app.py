4b3bb42e6a414692680fbb65ceb114b611cff8ba
Ravi <ravi87@gmail.com>
2010-07-08 19:30:04 -0700

    added about me

--- app.py
+++ app.py
@@ -37,8 +37,10 @@ class MyFrame(wx.Frame):
                
 		self.bktList = wx.ListBox(choices=gs.getbuckets(),parent=self,style=wx.LC_REPORT|wx.SUNKEN_BORDER,size=(200,100),pos=wx.Point(5,5))
                 self.bktList.Bind(wx.EVT_LISTBOX,self.OnListBox)
-                self.fileList = wx.ListBox(choices=[],parent=self,style=wx.LC_REPORT|wx.SUNKEN_BORDER,size=(400,100),pos=wx.Point(210,5))
-		self.fileList.Bind(wx.EVT_LISTBOX,self.Download)
+                self.fileList = wx.ListCtrl(parent=self,size=(400,100),pos=wx.Point(210,5))
+		self.fileList.InsertColumn(0,"Objects")
+		self.fileList.Bind(wx.EVT_LEFT_DOWN,self.Download)
+		self.fileList.Bind(wx.EVT_LIST_ITEM_RIGHT_CLICK,self.OnRightClick)
 		self.dirCtrl = wx.GenericDirCtrl(self,size=(200,200),pos=wx.Point(5,120))
 		self.dir_tree = self.dirCtrl.GetTreeCtrl()
 		self.dir_tree.SetWindowStyle(self.dir_tree.GetWindowStyle() | wx.TR_MULTIPLE)
@@ -49,40 +51,65 @@ class MyFrame(wx.Frame):
 		self.CreateStatusBar() #Create a status bar
 
 		#Setting up the menu
-		filemenu = wx.Menu()
-		menuItem = filemenu.Append(wx.ID_ANY,"Boto&Settings","View/Modify BotoSettings")
+		fileMenu = wx.Menu()
+		menuItem = fileMenu.Append(wx.ID_ANY,"Boto&Settings","View/Modify BotoSettings")
 		self.Bind(wx.EVT_MENU,self.OnBoto,menuItem)
-		filemenu.AppendSeparator()
-		menuItem = filemenu.Append(wx.ID_EXIT,"E&xit","Get outta here!")
+		fileMenu.AppendSeparator()
+		menuItem = fileMenu.Append(wx.ID_ANY,"&About","About this app")
+		self.Bind(wx.EVT_MENU,self.OnAbout, menuItem)
+		menuItem = fileMenu.Append(wx.ID_EXIT,"E&xit","Get outta here!")
 		self.Bind(wx.EVT_MENU,self.OnExit,menuItem)
 
 		#Setting up the menu bar
 		menuBar = wx.MenuBar()
-		menuBar.Append(filemenu,"&File")
+		menuBar.Append(fileMenu,"&File")
 		self.SetMenuBar(menuBar)
 		
 		self.Show(True)
+
+	def OnAbout(self,event):
+		wx.MessageBox("GSBrowser\nCode in wxPython\nR Raviprakash","About")
 
         def OnBoto(self,event):
                 self.botoFrame.Show(True)
 
         def OnListBox(self,event):
                 selName = self.bktList.GetStringSelection()
-                self.fileList.Set(gs.getobjects(selName))
+                for x in gs.getobjects(selName): self.fileList.InsertStringItem(0,x)
+		#self.fileList.Set(gs.getobjects(selName))
+
+	def OnRightClick(self,event):
+		selObj = event.GetText()
+		print selObj
+		popupMenu = wx.Menu()
+		menuItem = popupMenu.Append(wx.ID_ANY,"Delete","Delete this object")
+		self.Bind(wx.EVT_MENU,self.OnDelete,menuItem)
+		menuItem = popupMenu.Append(wx.ID_ANY,"Get Info","Get meta data")
+		self.Bind(wx.EVT_MENU,self.OnInfo,menuItem)
+		self.PopupMenu(popupMenu, event.GetPoint())
+		popupMenu.Destroy()
+		print "You right clicked!"
+
+	def OnInfo(self,event):
+		print "on info"
+	
+	def OnDelete(self,event):
+		print "delete"
 	
 	def Download(self,event):
 		bucketName = self.bktList.GetStringSelection()
-		fileName = self.fileList.GetStringSelection()
+		fileName = self.fileList.GetFirstSelected()
+		print fileName
 		dlg = wx.DirDialog(self, message="Pick a directory")
-		dlg.ShowModal()
-		dirName = dlg.GetPath()
-		self.StatusBar.SetStatusText("Downloading...")
-		gs.downloadObject(bucketName,fileName,dirName)
-		self.StatusBar.SetStatusText("Done!")
+		if dlg.ShowModal() != wx.ID_CANCEL:		
+			dirName = dlg.GetPath()
+			self.StatusBar.SetStatusText("Downloading...")
+			gs.downloadObject(bucketName,fileName,dirName)
+			self.StatusBar.SetStatusText("Done!")
 		dlg.Destroy()
 
 	def OnUpload(self,event):
-		print self.dirCtrl.GetFilePath()
+		print self.dirCtrl.GetFocusedItem()
 		bucketName = self.bktList.GetStringSelection()
 		fileName = self.dirCtrl.GetFilePath()
 		self.StatusBar.SetStatusText("Uploading...")
